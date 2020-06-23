30c49d75ffbd74683750a21a26801e59803177ac
Lubomir <lubomir.yudenko@gmail.com>
2020-06-23 13:56:26 +0300

    Move configs to env based variables

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ An automatic sandbox toggler, designed to be used with cron
 
 ## Getting started
 
-Before you get started, you must download chromium via `sudo apt install chromium`, `sudo pacman -S chromium`, or any other package manager.
+Before you get started, you must download chromium via `sudo apt install chromium-browser`, `sudo pacman -S chromium`, or any other package manager.
 
-It's very easy to use salesforce auto sandbox: Just download a release executable and fill in `config.json` by copying `config.example.json`. You also should export `AUTOSALESFORCE_ORDER` to the env. That's it! Now in an directory you can run `env AUTOSALESFOCE_ORDER=start ./salesforceautosandbox`, you can add that to cron or something now.
+It's very easy to use salesforce auto sandbox: Just download a release executable and fill in env variables. After that, you can just run it and it will do what you want! Now you can add it to cron or something.
+
+## Env variables
+
+|          Variable          |                       Description                       |
+| :------------------------: | :-----------------------------------------------------: |
+|      `CHROMIUM_PATH`       |            Path to your chromium executable             |
+| `AUTOSALESFORCE_CLIENTID`  |                       Client uuid                       |
+|   `AUTOSALESFORCE_LOGIN`   |                       User email                        |
+| `AUTOSALESFORCE_PASSWORD`  |                      User password                      |
+| `AUTOSALESFORCE_SANDBOXID` |                      Sandbox uuid                       |
+|   `AUTOSALESFORCE_ORDER`   | The order to send to the sandbox, `start`, `stop`, etc. |
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer")
-const config = require("./config.json")
+
+const debug = process.env.AUTOSALESFORCE_DEBUG === "debug"
 const baseTryItSelector =
 	"#operations-Sandboxes-post_sandboxes__sandboxId__operations>div"
 /**
@@ -9,23 +10,51 @@ const baseTryItSelector =
 function wait(time) {
 	return new Promise((resolve) => setTimeout(resolve, time))
 }
+if (debug)
+	console.log(
+		process.env.CHROMIUM_PATH,
+		process.env.AUTOSALESFORCE_CLIENTID,
+		process.env.AUTOSALESFORCE_LOGIN,
+		process.env.AUTOSALESFORCE_PASSWORD,
+		process.env.AUTOSALESFORCE_SANDBOXID,
+		process.env.AUTOSALESFORCE_ORDER
+	)
 ;(async () => {
+	if (
+		!(
+			process.env.CHROMIUM_PATH &&
+			process.env.AUTOSALESFORCE_CLIENTID &&
+			process.env.AUTOSALESFORCE_CLIENTID &&
+			process.env.AUTOSALESFORCE_PASSWORD &&
+			process.env.AUTOSALESFORCE_SANDBOXID &&
+			process.env.AUTOSALESFORCE_ORDER
+		)
+	) {
+		throw new Error(
+			"Env missing required variables! Read the README for required variables"
+		)
+	}
+
 	const browser = await puppeteer.launch({
-		executablePath: "/usr/bin/chromium",
+		executablePath: process.env.CHROMIUM_PATH,
+		headless: !debug,
+		slowMo: debug ? 100 : 0,
 	})
+
 	const page = await browser.newPage()
 	await page.goto("https://admin.us01.dx.commercecloud.salesforce.com/")
 	await (await page.waitForSelector(".btn,authorize")).click()
 	let input = (await page.waitForSelector("#client_id")).asElement()
-	await input.type(config.clientId)
+	await input.type(process.env.AUTOSALESFORCE_CLIENTID)
 	await (await page.waitForSelector(".modal-btn")).click()
 	await wait(2000)
+	page.$
 	//Switch to auth tab
 	const authTab = (await browser.pages())[2]
 	input = (await authTab.waitForSelector("#idToken1")).asElement()
-	await input.type(`${config.login}\n`)
+	await input.type(`${process.env.AUTOSALESFORCE_LOGIN}\n`)
 	input = (await authTab.waitForSelector("#idToken2")).asElement()
-	await input.type(`${config.password}\n`)
+	await input.type(`${process.env.AUTOSALESFORCE_PASSWORD}\n`)
 	await wait(3000)
 	//Go back to main page and do orders
 	await (await page.waitForSelector(".close-modal")).click()
@@ -35,7 +64,7 @@ function wait(time) {
 			`${baseTryItSelector}>div>div>.opblock-section-header>div>button`
 		)
 	).click()
-	const operation = process.env.AUTOSALESFORCE_ORDER || "stop"
+	const operation = process.env.AUTOSALESFORCE_ORDER
 	await page.evaluate(
 		(sel) => (document.querySelector(sel).value = ""),
 		`${baseTryItSelector}>div>div>div>table>tbody>tr>td>div>div>div>textarea`
@@ -49,7 +78,7 @@ function wait(time) {
 		await page.waitForSelector(
 			`${baseTryItSelector}>div>div>div>table>tbody>tr>td>input`
 		)
-	).type(config.sandboxId)
+	).type(process.env.AUTOSALESFORCE_SANDBOXID)
 	await (
 		await page.waitForSelector(`${baseTryItSelector}>div>div>.execute`)
 	).click()
